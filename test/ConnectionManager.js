90946ef23c29058b09c9cfe8f66d1a51275314e1
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-16 15:22:31 +0530

    init

--- test/ConnectionManager.js
+++ test/ConnectionManager.js
@@ -2,30 +2,9 @@ module.exports=function(){
 
     this.dbConnections = [];
     
-    this.dbConnections["test3"] = {
-        host: "test3.cvmlduwb8e2g.us-east-1.rds.amazonaws.com",
-        port: "3306",
-        user: "rdsuser",
-        password: "12345678",
-        database: "firstDB",
-    };
-    this.dbConnections["aurora_testa"] = {
-        host: process.env.EndPoint_aurora_testa,
-        port: ,
-        user: "aurora_testa",
-        password: "12345678",
-        database: "aurora_testa",
-    };
     this.dbConnections["aurora_test"] = {
         host: process.env.EndPoint_aurora_test,
-        port: ,
-        user: "aurora_test",
-        password: "12345678",
-        database: "aurora_test",
-    };
-    this.dbConnections["aurora_test"] = {
-        host: process.env.EndPoint_aurora_test,
-        port: ,
+        port: 3306,
         user: "aurora_test",
         password: "12345678",
         database: "aurora_test",
