118fd22d94012ee84c50dcea66607b32dd54b1b7
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-16 14:51:55 +0530

    init

--- test/test-lambda.js
+++ test/test-lambda.js
@@ -1,32 +1,16 @@
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
-const rds = new SL.RDS(connectionManager);
 let SL = require('slappforge-aws');
-const sqs = new SL.AWS.SQS();
+const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
-	sqs.receiveMessage({
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
-		AttributeNames: ['All'],
-		MaxNumberOfMessages: '1',
-		VisibilityTimeout: '30',
-		WaitTimeSeconds: '0'
-	}, function (receivedMessages) {
-		receivedMessages.forEach(message => {
-			// your logic to access each message through out the loop. Each message is available under variable message 
-			// within this block
-		})
-	}, function (error) {
-		// implement error handling logic here
-	});
-
-
 
 	// Replace the query with the actual query
+	// You can pass the existing connection to this function.
+	// A new connection will be creted if it's not present as the third param 
 	rds.query({
-		identifier: 'test3',
-		query: '<query>',
-		transactional: false
-	}, function (error, results, fields) {
+		identifier: 'aurora_test',
+		query: '<query>'
+	}, function (error, results, connection) {
 		if (error) {
 			console.log("Error occurred");
 			throw error;
@@ -34,6 +18,10 @@ exports.handler = function (event, context, callback) {
 			console.log("Success")
 			console.log(results);
 		}
+
+		connection.end();
 	});
+
+
 	callback(null, 'Successfully executed');
 }
