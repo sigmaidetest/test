1567c115d6d831ad825e4419f46f72e05855eb3b
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-29 15:40:37 +0530

    qdwqd

--- test/lambda.js
+++ test/lambda.js
@@ -1,6 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
-sdsdsdsds
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
-	callback(null,'Successfully executed');
+	sns.unsubscribe({
+		SubscriptionArn: 'arn:aws:sns:us-east-1:480964559519:ADRT:123444'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+
+
+	callback(null, 'Successfully executed');
 }
